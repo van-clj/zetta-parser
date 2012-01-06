1bf188b3ba60f8a90e8b8a2553d8304d3058ec8d
Roman Gonzalez <romanandreg@gmail.com>
2012-01-05 19:31:50 -0800

    Adding descriptions to the project.clj file

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject zetta-parse "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Powerful monadic parser combinator for Clojure"
+  :author "Roman Gonzalez"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.1-SNAPSHOT"]])
