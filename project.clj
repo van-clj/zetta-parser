39bfcdf34f569942c23935925625383b2c1424c6
Roman Gonzalez <romanandreg@gmail.com>
2012-02-06 12:32:28 -0800

    algo.monads-1.0.3 SNAPSHOT version, there is no stable yet

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Powerful monadic parser combinator for Clojure"
   :author "Roman Gonzalez"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/algo.monads "0.1.3"]])
+                 [org.clojure/algo.monads "0.1.3-SNAPSHOT"]])
