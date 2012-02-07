715376b3de5d14ca5f672a72ba8165e09805e569
Roman Gonzalez <romanandreg@gmail.com>
2012-02-07 10:31:27 -0800

    Fixing version of org.clojure/algo.monads dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject zetta-parser "0.0.1-SNAPSHOT"
   :description "Powerful monadic parser combinator for Clojure"
   :author "Roman Gonzalez"
+  :repositories { "sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots/"}}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/algo.monads "0.1.3-SNAPSHOT"]]
+                 [org.clojure/algo.monads "0.1.3-20120206.105742-1"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]])
