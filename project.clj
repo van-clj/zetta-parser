9f116cbcd0790bb51448bb3c460f4872a748467e
ujihisa <ujihisa@gmail.com>
2012-09-30 21:38:13 -0700

    started developing version 0.0.5
    * with clojure 1.4.0
    * algo.monads changed where it belongs to

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.van-clj/zetta-parser "0.0.4-SNAPSHOT"
+(defproject org.van-clj/zetta-parser "0.0.5-SNAPSHOT"
   :description "Powerful monadic parser combinator in Clojure (Haskell attoparsec's port)"
   :author "Roman Gonzalez"
   :warn-on-reflection true
   :repositories { "sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots/"}}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/algo.monads "0.1.3-20120206.105742-1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [psy.ai/algo.monads "0.1.3-SNAPSHOT"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]
                      [marginalia "0.7.0-SNAPSHOT"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]])
