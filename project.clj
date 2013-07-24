3d22c7b4b0e324caff386373b10f8ab3510eab1d
Roman Gonzalez <romanandreg@gmail.com>
2013-07-23 18:20:47 -0700

    Bump to 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.van-clj/zetta-parser "0.0.4-SNAPSHOT"
+(defproject org.van-clj/zetta-parser "0.0.4"
   :description "Powerful monadic parser combinator in Clojure (Haskell attoparsec's port)"
   :author "Roman Gonzalez"
   :warn-on-reflection true
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.monads "0.1.3-SNAPSHOT"]]
+                 [org.clojure/algo.monads "0.1.4"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]
                      [marginalia "0.7.0-SNAPSHOT"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]])
