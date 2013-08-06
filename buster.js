32c1c4551bf937f407e0c71eb84bd48a02479ae0
Roman Gonzalez <romanandreg@gmail.com>
2013-08-05 23:19:56 -0700

    Setup project to use nodejs tools for testing

---
+++ buster.js
@@ -0,0 +1,13 @@
+var config = exports;
+
+config['Browser Tests'] = {
+    environment: 'browser',
+    sources: [],
+    tests: [ "resources/js/zetta-parser-browser-test.js" ]
+};
+
+config['Node Tests'] = {
+    environment: 'node',
+    sources: [],
+    tests: []
+};
--- dalap_rules.clj
+++ dalap_rules.clj
@@ -1,13 +1,29 @@
-{ ["src/clj/zetta/parser/core.clj"
-   "src/cljs/zetta/parser/core.cljs"]
-  {}
-  ["src/clj/zetta/parser/seq.clj"
-   "src/cljs/zetta/parser/seq.cljs"]
-  {}
-  ["src/clj/zetta/parser/string.clj"
-   "src/cljs/zetta/parser/string.cljs"]
-  {}
-  ["src/clj/zetta/parser/combinators.clj"
-   "src/cljs/zetta/parser/combinators.cljs"]
-  {}
+{ ["src/clj/zetta/parser/core.clj" "src/cljs/zetta/parser/core.cljs"]
+  []
+
+  ["src/clj/zetta/parser/combinators.clj" "src/cljs/zetta/parser/combinators.cljs"]
+  []
+
+  ["src/clj/zetta/parser/seq.clj" "src/cljs/zetta/parser/seq.cljs"]
+  []
+
+  ["src/clj/zetta/parser/string.clj" "src/cljs/zetta/parser/string.cljs"]
+  []
+
+  ["test/clj/zetta/tests/parser/combinators_test.clj"
+   "test/cljs/zetta/tests/parser/combinators_test.cljs"]
+  []
+
+
+  ["test/clj/zetta/tests/parser/seq_test.clj"
+   "test/cljs/zetta/tests/parser/seq_test.cljs"]
+  []
+
+  ["test/clj/zetta/tests/parser/core_test.clj"
+   "test/cljs/zetta/tests/parser/core_test.cljs"]
+  []
+
+  ["test/clj/zetta/tests/parser/string_test.clj"
+   "test/cljs/zetta/tests/parser/string_test.cljs"]
+  []
  }
