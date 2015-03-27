73fc2cea3bf561459004974dd534a67a44bd5eea
Roman Gonzalez <roman@unbounce.com>
2015-03-27 14:18:43 -0700

    Bump project to v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.van-clj/zetta-parser "0.1.0-SNAPSHOT"
+(defproject org.van-clj/zetta-parser "0.1.0"
   :description "Powerful monadic parser combinator in Clojure (Haskell attoparsec's port)"
   :author "Roman Gonzalez"
   :repositories { "sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots/"}}
